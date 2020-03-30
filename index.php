844aaf2b47d4dfc5181bf97ff2b45fd1e4008f88
MartinVecera <36948869+MartinVecera@users.noreply.github.com>
2020-03-30 12:28:12 +0200

    upravy

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Document</title>
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/css/uikit.min.css" />
+    <!-- UIkit JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit-icons.min.js"></script>
+</head>
+<body>
+
+
+    
+</body>
+</html>
+
