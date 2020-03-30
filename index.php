1725ffa60900519b917224852c0569038f74518e
MartinVecera <36948869+MartinVecera@users.noreply.github.com>
2020-03-30 18:48:07 +0200

    upravy

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
-
+include("tridy.php");
 
 ?>
 
 <!DOCTYPE html>
+<html lang='cs'>
 <head>
-    <meta charset='UTF-8'>
+<meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <title>Document</title>
@@ -18,7 +19,17 @@
 </head>
 <body>
 
-
+<p>Postava <?php echo $properties->race ?></br>
+<?php echo "Pohlaví: " . $properties->sex ?></br>
+<?php echo "Jméno: " . $properties->name ?></br>
+<?php echo "Příjmení: " . $properties->surname ?></br>
+<?php echo "Životy, odolnost: " . $properties->stamina ?></br>
+<?php echo "Síla: " . $properties->strenght ?></br>
+<?php echo "Moudrost: " . $properties->wisdom ?></br>
+<?php echo "Inteligence: " . $properties->inteligence ?></br>
+<?php echo "Charisma: " . $properties->charisma ?></br>
+<?php echo "Obratnost: " . $properties->dexerity ?></br>
+</p>
     
 </body>
 </html>
--- tridy.php
+++ tridy.php
@@ -1,5 +1,71 @@
 <?php
 
+class Properties 
+{
+
+    public $stamina;
+    public $strenght;
+    public $wisdom;
+    public $inteligence;
+    public $charisma;
+    public $dexerity;
+    public $sex;
+    public $name;
+    public $surname;
+
+    public function __construct($stamina, $strenght, $wisdom, $inteligence, $charisma, $dexerity, $sex, $race)
+    {
+
+        $this->stamina = $stamina;
+        $this->strenght = $strenght;
+        $this->wisdom = $wisdom;
+        $this->inteligence = $inteligence;
+        $this->charisma = $charisma;
+        $this->dexerity = $dexerity;
+        $this->sex = $sex;
+        $this->race = $race;
+
+        $this->name = $this->name();
+        $this->surname = $this->surname();
+
+    }
+
+    public function name ()
+    {
+
+        $male_names = array("Adrik", "Alberich", "Baer", "Barendd", "Brottor", "Dain", "Darrak", "Eberk", "Einkil", "Fargrim", "Gardain", "Harbek", "Kildrak", "Morgran", "Orsik", "Oskar", "Rangrim", "Rurik", "Taklinn", "Toradin", "Torin", "Tordek", "Traubon", "Travok", "Ulfgar", "Veit", "Vondal");
+        $female_names = array("Artina", "Audhilda", "Bardryna", "Dagnala", "Diesa", "Eldeta", "Falkunna", "Gunnloda", "Gurdisa", "Helja", "Hlina", "Katra", "Kristryda", "Ildea", "Liftrasa", "Mardreda", "Rtiswynna", "Sannla", "Torbera", "Torgga", "Vistra");
+
+        if ($this->sex == "male") {
+            $random_name = array_rand($male_names);
+            return $male_names[$random_name];
+        } else if ($this->sex = "female") {
+            $random_name= array_rand($female_names);
+            return $female_names[$random_name];
+        }
+    }
+
+    public function surname ()
+    {
+
+        $surnames = array("Balderk", "Dankil", "Gorunn", "Holderhek", "Loderr", "Lutgehr", "Rumnaheim", "Strakeln", "Torunn", "Ungart");
+
+        $random_surnames = array_rand($surnames);
+        return $surnames[$random_surnames];
+
+    }
+
+}
+
+Class Race extends Properties
+{
+
+    public $race;
+
+}
+
+$properties = new Properties(200, 150, 80, 200, 100, 130, "male", "Trpaslík");
+
 
 
 ?>
